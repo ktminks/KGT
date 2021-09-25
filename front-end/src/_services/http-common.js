7e8a7af4b0cd9fe3fa55ef7b60dfb386d7b43f37
ktminks <katie.minckler@gmail.com>
2021-09-24 22:24:04 -0400

    nginx error

--- front-end/src/_services/http-common.js
+++ front-end/src/_services/http-common.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const mode = process.env.MODE ? process.env.MODE : "DEVELOPMENT";
+const URL =
+  mode === "PRODUCTION"
+    ? "https://kgt.ktminks.com/api"
+    : "http://localhost:4000/api";
+
 export default axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: URL,
   headers: {
     "Content-type": "application/json",
   },
--- front-end/src/_services/http-common.production.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://kgt.ktminks.com/api",
-  headers: {
-    "Content-type": "application/json",
-  },
-});
