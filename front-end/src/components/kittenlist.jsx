1b6e30d9503f9e44b763e1016234d03ebc754de4
ktminks <katie.minckler@gmail.com>
2021-09-26 05:33:44 -0400

    front-end issue

--- front-end/src/components/kittenlist.jsx
+++ front-end/src/components/kittenlist.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import KittenListItem from "./kittenlistitem";
 
 const KittenList = ({ kittens, currentIndex, setActiveKitten }) => {
   // componentDidUpdate(prevProps) {
