6324bf50e683f567e0a6d47e29fd469510e95f53
ktminks <katie.minckler@gmail.com>
2021-11-28 23:28:59 -0500

    ECR update

--- back-end/_helpers/setSession.js
+++ back-end/_helpers/setSession.js
@@ -5,10 +5,10 @@ export default function setSession(app, client) {
   const clientPromise = new Promise((res) => res(client));
 
   app.use(session({
-    secret: process.env.SESSION_SECRET || "catballs",
+    secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-      httpOnly: false,
+      httpOnly: true,
       secure: "auto",
     },
     resave: true,
