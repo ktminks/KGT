28c0d0ef3dfee8735d1742c3fcb10321e212e10b
KT Minckler <katie.minckler@gmail.com>
2021-12-03 05:14:53 -0500

    backend deployment

--- back-end/_helpers/__tests__/checkMode.test.js
+++ back-end/_helpers/__tests__/checkMode.test.js
@@ -4,7 +4,6 @@ describe("checkMode", () => {
   it("should return development", () => {
     expect.assertions(1);
     const mode = checkMode();
-    // console.log(mode);
     expect(mode).toBe("development");
   });
 });
